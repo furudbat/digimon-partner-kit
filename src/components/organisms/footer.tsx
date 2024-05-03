01489f0e2fc00e8ed8e127dc480152ecf12c0b22
furudbat <hircreacc@gmail.com>
2024-05-03 16:43:21 +0200

    fix: footer

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link';
 
 export function MainFooter() {
   return (
-    <Footer container className="footer bottom-0 inset-x-0 rounded-none shadow-none max-h-12 mt-4 lg:mt-6">
+    <Footer container className="footer bottom-0 inset-x-0 rounded-none shadow-none h-14 mt-4 md:mt-6 lg:mt-8">
       <div className="mx-6 w-full m-4">
-        <div className="sm:flex items-center sm:justify-between w-full">
+        <div className="sm:flex items-center sm:justify-between w-full h-14">
           <span className="text-sm text-gray-500 dark:text-gray-400">
             © 2024{' '}
             <Link href="https://github.com/furudbat" target="_blank" rel="noreferrer" className="hover:underline">
